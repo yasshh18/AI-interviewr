ced2f2df5782fbc70057f594dbd45d510954e7ca
yash chaurasiya <yashchaurasiya@yashs-MacBook-Air.local>
2026-06-10 10:13:43 +0530

    Fix API URLs

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -20,7 +20,7 @@ function Register() {
       return
     }
     try {
-      const response = await fetch('http://localhost:5000/api/auth/register', {
+      const response = await fetch('https://ai-interviewr.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ name, email, password })
